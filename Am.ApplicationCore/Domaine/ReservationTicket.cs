d27a794361f1ef70b4e71521b6b3d21398ebf702
Mahdi1-1 <Masmoudi.Mahdi@esprit.tn>
2026-05-11 11:23:26 +0100

    ajouter ticket

---
+++ Am.ApplicationCore/Domaine/ReservationTicket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Am.ApplicationCore.Domaine
+{
+    public class ReservationTicket
+
+    {
+        public string Date { get; set; }
+        public float prix { get; set; }
+        [ForeignKey("Ticket")]
+        public int TicketFk { get; set; }
+        [ForeignKey("Passenger")]
+        public int PassengerFk { get; set; }
+        public Ticket Ticket { get; set; }
+        public Passenger Passenger { get; set; }
+
+
+    }
+}
---
+++ Am.ApplicationCore/Domaine/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Am.ApplicationCore.Domaine
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Classe { get; set; }
+        public string Destination { get; set; }
+        public ICollection<ReservationTicket> ReservationTicket { get; set; }
+        public Ticket Ticket { get; set; }
+        public Passenger Passenger { get; set; }
+
+
+    }
+}
